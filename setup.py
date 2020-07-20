b233445750029b2a5412c3b3098a13481b82534b
katznboyz1 <harrison.lindgren@gmail.com>
2020-07-20 16:12:27 -0400

    Added code to setup.py to initialize the app's database

--- setup.py
+++ setup.py
@@ -1,10 +1,38 @@
 #import statements
-import json
+import json, sqlite3, os
 
 #save the details to a config file
 configFileData = {
     'application_name':str(input('What would you like your youtube-dl-server application to be named? '))
 }
+
+#check if there is already a database
+if (os.path.exists('./youtube-dl-server-database.db')):
+
+    #tell the user that there is a database, then exit. the user will then do what they need to do with the old database.
+    print('There already is an existing database! Please move the old database to another directory, or if you are trying to use the old database again, rename it to "youtube-dl-server-database.db.old", then run the setup program again, and then replace the new database with the old database. Please keep in mind that importing a database from an older version of the program can lead to errors, if its not converted properly!')
+    exit()
+
+#create the database
+DATABASE_CONNECTION = sqlite3.connect('./youtube-dl-server-database.db')
+
+#make the users table
+DATABASE_CONNECTION.execute('''
+CREATE TABLE users (
+    username VARCHAR NOT NULL PRIMARY KEY,
+    password VARCHAR NOT NULL,
+    admin BOOL NOT NULL DEFAULT 0
+)
+''')
+
+#make the download history table (i dont think 8 bit variables will be nessecary for most applications, however if they are nessecary for you, then replace SERIAL with SERIAL8 and INT with INT8)
+DATABASE_CONNECTION.execute('''
+CREATE TABLE download_history (
+    download_id SERIAL NOT NULL PRIMARY KEY,
+    url VARCHAR NOT NULL,
+    status INT NOT NULL
+)
+''')
 
 #write the config file
 configFile = open('./config.json', 'w')
