4b33c1d509cb2e58d7283bae392150b27d74b61a
katznboyz1 <harrison.lindgren@gmail.com>
2020-07-20 13:11:34 -0400

    Reworked the setup file to begin allowing downloading to other directories

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import json
 
 #save the details to a config file (maybe find another way to store them than plaintext?)
 configFileData = {
-    'application_name':SECRET_URL_APP_TITLE
+    'application_name':str(input('What would you like your youtube-dl-server application to be named? '))
 }
 
 #write the config file
@@ -11,7 +11,18 @@ configFile = open('./config.json', 'w')
 configFile.write(json.dumps(configFileData))
 configFile.close()
 
+#make the download directory file (the file where all of the optional download directories are stored)
+downloadDirsFile = open('./download-dirs.txt', 'w')
+downloadDirsFile.write('''
+#Write the absolute path to the directory that you want users to be able to download videos to, sperated by new lines.
+#You can comment out entries by starting the line with a pound sign.
+#Paths with spaces are supported.
+#If no paths are added in this file, then the user will only be able to download to the downloads folder that comes with this program, instead of being able to download to other directories on the server.
+#Example lines:
+#/home/greg/videos/
+#/data/jellyfin/My Videos/Linus Tech Tips/WAN Show/
+#/media/mary/flask-drive-videos/
+''')
+
 #tell the user information about the config
-print('''
-Setup is complete!
-'''.format(RANDOM_SECRET_KEY))
+print('Setup is complete!')
