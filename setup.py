2a164127765afcac9af8d07f4ce5ec246815a0a8
katznboyz1 <harrison.lindgren@gmail.com>
2020-07-30 16:08:04 -0400

    Removed old useless code

--- setup.py
+++ setup.py
@@ -85,19 +85,6 @@ configFile = open('./config.json', 'w')
 configFile.write(json.dumps(configFileData))
 configFile.close()
 
-#make the download directory file (the file where all of the optional download directories are stored)
-downloadDirsFile = open('./download-dirs.txt', 'w')
-downloadDirsFile.write('''
-#Write the absolute path to the directory that you want users to be able to download videos to, sperated by new lines.
-#You can comment out entries by starting the line with a pound sign.
-#Paths with spaces are supported.
-#If no paths are added in this file, then the user will only be able to download to the downloads folder that comes with this program, instead of being able to download to other directories on the server.
-#Example lines:
-#/home/greg/videos/
-#/data/jellyfin/My Videos/Linus Tech Tips/WAN Show/
-#/media/mary/flask-drive-videos/
-''')
-
 #make the file that contains the list of proxies
 proxiesFile = open('./proxies.txt', 'w')
 proxiesFile.write('''
