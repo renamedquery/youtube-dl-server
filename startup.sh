049078114a4d35ea1b552cb941b015e63b50303c
Plaidstallion <44079566+Plaidstallion@users.noreply.github.com>
2021-03-16 15:31:40 +0100

    Update startup.sh
    chown the working dir on startup
    Use s6-setuidgid to start everything as the user abc with the UID:GID 1000:1000

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # Directly referencing these variables now because docker-compose sucks
-python3 setup.py --appname $APPNAME --username $ADMINUSER --password $PASSWORD
-gunicorn --workers 4 --threads 4 --bind 0.0.0.0:8080 wsgi:app
+chown -R abc:abc /app
+s6-setuidgid abc python3 setup.py --appname $APPNAME --username $ADMINUSER --password $PASSWORD
+s6-setuidgid abc gunicorn --workers 4 --threads 4 --bind 0.0.0.0:8080 wsgi:app
