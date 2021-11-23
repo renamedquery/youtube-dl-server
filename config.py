2454160b0e882590f983a6a21d524b19d103586a
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-23 14:36:13 +0000

    fixed /old contents

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-# Check for legacy DB to not break anything
-if (os.path.exists('./youtube-dl-server-database.db')):
-    DATABASE_PATH = ('./youtube-dl-server-database.db')
-else:
-    DATABASE_PATH = os.path.join('db' + os.sep + 'youtube-dl-server-database.db')
-# LEGACY DATABASE_PATH
-#DATABASE_PATH = ('./youtube-dl-server-database.db')
