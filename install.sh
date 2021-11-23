f86a33535840df0967e6b5178307821b87aebd0a
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-23 15:26:58 +0000

    added a downloader for treefrog to install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,22 @@ apt-get install -y qt5-default qt5-qmake libqt5sql5-mysql libqt5sql5-psql libqt5
 
 # db client libs
 apt-get install -y libmysqlclient-dev libpq5 libodbc1 libmongoc-dev libbson-dev
+
+# download treefrog to the current dir
+wget https://github.com/treefrogframework/treefrog-framework/archive/v2.2.0.tar.gz
+
+# unzip treefrog
+tar xvfz treefrog-2.2.0.tar.gz
+
+# build treefrog
+cd treefrog-x.x.x
+./configure
+cd src
+make
+sudo make install
+cd ../tools
+make
+sudo make install
+
+# allow debugging (temporarily)
+./configure --enable-debug
